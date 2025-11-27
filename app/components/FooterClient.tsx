1c6ecfd9f41699851dc5c85e4c4d73982e55b216
jesus caceres <jesus.caceres@email.com>
2025-11-27 10:59:54 -0800

    push

--- app/components/FooterClient.tsx
+++ app/components/FooterClient.tsx
@@ -4,7 +4,9 @@ import { useSearchParams } from "next/navigation";
 import Footer from "./Footer";
 
 export default function FooterClient() {
+  // We still read the language, but we do NOT pass it to Footer
   const searchParams = useSearchParams();
   const lang = searchParams.get("lang") === "en" ? "en" : "es";
-  return <Footer lang={lang} />;
+
+  return <Footer />;
 }
