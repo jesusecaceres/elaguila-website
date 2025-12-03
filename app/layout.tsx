f065032a6ade24c68fbc652fa67bb97b9421bc29
jesus caceres <jesus.caceres@email.com>
2025-12-02 16:39:30 -0800

    commit and push

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,13 +3,24 @@
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import { Suspense } from "react";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body>
-        <Navbar />
-        {children}
+        <Suspense fallback={null}>
+          <Navbar />
+        </Suspense>
+
+        <Suspense fallback={null}>
+          {children}
+        </Suspense>
+
         <Footer />
       </body>
     </html>
