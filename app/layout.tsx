087586e53f87f23cd51d11a0d52df75bfce4124d
jesus caceres <jesus.caceres@email.com>
2025-12-05 17:48:33 -0800

    commit and push

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,35 +12,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className="bg-black text-white">
-        {/* Navbar always visible except intro */}
-        <Navbar />
-
-        {/* Page content */}
-        <Suspense fallback={<div>Loading...</div>}>
-          {children}
-        </Suspense>
-
-        {/* Footer */}
-        <Footer />
-      </body>
-    </html>
-  );
-}
-import "./globals.css";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import { Suspense } from "react";
-
-export const metadata = {
-  title: "El Águila en Vuelo",
-  description: "Orgullo Latino Sin Fronteras",
-};
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body className="bg-black text-white">
-        {/* Navbar always visible except intro */}
+        {/* Navbar */}
         <Navbar />
 
         {/* Page content */}
